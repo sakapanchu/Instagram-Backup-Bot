6abc27022915d89e5b0d335b3eeeb0f45c9be025
sakaniya panchalingam <117504870+sakapanchu@users.noreply.github.com>
2024-06-07 15:32:12 +0800

    Add files via upload

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,7 +1,10 @@
 import instaloader
 
+# Create an instance of Instaloader
 insta = instaloader.Instaloader()
 
+# Specify the Instagram account username
 acc = 'opswatacademy'
 
+# Download the profile data
 insta.download_profile(acc, profile_pic_only=False)
